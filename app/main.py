d8f2f03c3ca72efc2691e7c27bf7b10f46c1f70f
Penguincible <illya.ivanov@tutanota.com>
2026-04-20 11:05:25 -0600

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,7 +8,10 @@ def main():
     # Uncomment the code below to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    connection, _ = server_socket.accept() # _ is used because .accept() returns two values
+    connection.sendall(b"+PONG\r\n")
+    #connection, shm = server_socket.accept() # _ is used because .accept() returns two values
+    #print(connection, shm) #the .accept() doesn't happen on it's own, makes sense
 
 
 if __name__ == "__main__":

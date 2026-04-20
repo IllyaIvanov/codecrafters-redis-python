2c27490d83fe3629bb0307a47fcd8cce171bb02d
Penguincible <illya.ivanov@tutanota.com>
2026-04-20 11:38:11 -0600

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,24 +1,26 @@
 import socket  # noqa: F401
+import threading
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+    def respond(conn):
+        data = conn.recv(1024)
+        if data:
+            conn.sendall(b"+PONG\r\n")
 
-    # Uncomment the code below to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    #connection, shm = server_socket.accept() # _ is used because .accept() returns two values
-    #print(connection, shm) #the .accept() doesn't happen on it's own, makes sense
-    connections = []
+    threads = []
     while True:
         connection, _ = server_socket.accept() # _ is used because .accept() returns two values
-        connections.append(connection)
-        print(connection)
-        for curcon in connections:
-            data = curcon.recv(1024)
-            if data:
-                curcon.sendall(b"+PONG\r\n")
+        if connection:
+            threads.append(thr)
+            thr = threading.Thread(target=respond, args=(connection))
+            thr.start
+        
+        #for curcon in connections:
+        #    data = curcon.recv(1024)
+        #    if data:
+        #        curcon.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":

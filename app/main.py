613f62f48f0a1f48d07eff3414dae326e52a2358
Penguincible <illya.ivanov@tutanota.com>
2026-04-20 11:23:32 -0600

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,15 +8,13 @@ def main():
     # Uncomment the code below to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, _ = server_socket.accept() # _ is used because .accept() returns two values
     #connection, shm = server_socket.accept() # _ is used because .accept() returns two values
     #print(connection, shm) #the .accept() doesn't happen on it's own, makes sense
     while True:
+        connection, _ = server_socket.accept() # _ is used because .accept() returns two values
         data = connection.recv(1024)
         if data:
             connection.sendall(b"+PONG\r\n")
-            data = None
-            break
 
 
 if __name__ == "__main__":

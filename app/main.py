9c4c3d5c07ec523f2ebc191db2cb85eadc5fb03a
Penguincible <illya.ivanov@tutanota.com>
2026-04-20 11:47:18 -0600

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,17 +5,18 @@ import threading
 def main():
     def respond(conn):
         data = conn.recv(1024)
-        if data:
-            conn.sendall(b"+PONG\r\n")
+        if not data:
+            break
+        conn.sendall(b"+PONG\r\n")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    threads = []
+
     while True:
         connection, _ = server_socket.accept() # _ is used because .accept() returns two values
-        if connection:
-            thr = threading.Thread(target=respond, args=(connection,))
-            threads.append(thr)
-            thr.start()
+        if not connection:
+            break
+        thr = threading.Thread(target=respond, args=(connection,))
+        thr.start()
         
         #for curcon in connections:
         #    data = curcon.recv(1024)
